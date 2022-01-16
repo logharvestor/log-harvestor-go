1c3ae27e9e372ae0eadc611abef673d96b86fc72
solomon <solomon@logharvestor.com>
2022-01-16 15:48:04 -0700

    1.0.3 - Patch Version

--- constants.go
+++ constants.go
@@ -2,7 +2,7 @@ package logharvestorgo
 
 const (
 	// Client Version
-	LogHarvestorClientVersion = "Go-1.0.2"
+	LogHarvestorClientVersion = "Go-1.0.3"
 	// Default Configs
 	ApiUrl  = "https://app.logharvestor.com/api/log"
 	Token   = ""
